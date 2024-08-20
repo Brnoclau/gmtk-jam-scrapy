8febbd13c06f8dcfcd0c697f841e56b411e222e2
Uladzimir Malhin <malhin.vladimir@gmail.com>
2024-08-20 10:19:53 +0200

    MultiDialogObject

---
+++ Assets/Scripts/Dialogs/MultiDialogObject.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Scrapy
+{
+    public class MultiDialogObject : Interactable
+    {
+        [SerializeField] private DialogConfig defaultDialogConfig;
+        [SerializeField] private List<MultiDialogOption> options;
+        
+        public override void Interact()
+        {
+            var dialog = GetCurrentOption();
+            if (dialog != null)
+            {
+                DialogManager.Instance.StartDialog(dialog.dialogConfig);
+            }
+            else
+            {
+                DialogManager.Instance.StartDialog(defaultDialogConfig);
+            }
+        }
+
+        public MultiDialogOption GetCurrentOption()
+        {
+            // from last to first check required checkpoints
+            for (int i = options.Count - 1; i >= 0; i--)
+            {
+                var option = options[i];
+                if (option.requiredCheckpoints.Count == 0)
+                {
+                    return option;
+                }
+                bool allRequired = true;
+                foreach (var checkpoint in option.requiredCheckpoints)
+                {
+                    if (!QuestManager.Instance.CompleteCheckpoints.Contains(checkpoint))
+                    {
+                        allRequired = false;
+                        break;
+                    }
+                }
+
+                if (allRequired)
+                {
+                    return option;
+                }
+            }
+
+            return null;
+        }
+    }
+
+    [Serializable]
+    public class MultiDialogOption
+    {
+        public List<string> requiredCheckpoints;
+        public DialogConfig dialogConfig;
+    }
+}
